0e0e86c2698a7a0ca451185ca60adb5e6e62139c
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-09-24 21:07:44 +0200

    Try to fix the workflow and trigger the workflow
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_init_print.sh
+++ .github/scripts/common_init_print.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-echo "threads=${{ steps.param.outputs.threads }}"
 
-echo "dir_list_dev=${{ steps.param.outputs.dir_list_dev }}"
-echo "dir_list_test=${{ steps.param.outputs.dir_list_test }}"
-echo "dir_list_prod=${{ steps.param.outputs.dir_list_prod }}"
-
-echo "execute_check_and_format=${{ steps.param.outputs.execute_check_and_format }}"
-echo "execute_test=${{ steps.param.outputs.execute_test }}"
-echo "execute_deploy=${{ steps.param.outputs.execute_deploy }}"
-
-echo "sha7=${{ steps.param.outputs.sha7 }}"
-echo "tmp_git_branch=${{ steps.param.outputs.tmp_git_branch }}"
-echo "cache_key=${{ steps.param.outputs.cache_key }}"
