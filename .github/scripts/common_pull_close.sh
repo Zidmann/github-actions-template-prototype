2e5446764ad7f3c263147a16556686d169c09386
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-08 14:37:45 +0200

    Try to fix the script
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_pull_close.sh
+++ .github/scripts/common_pull_close.sh
@@ -2,10 +2,6 @@ if [[ "$PR_URL" == "" ]] && [[ "$PR_NUMBER" == "" ]]
 then
 	echo "[i] No changes in the code, deployment are allowed"
 else
-	echo "[i] Configuring the Github settings"
-	git remote add originssh "git@github.com:$GITHUB_REPOSITORY.git"
-	git push originssh --delete "$BRANCH_NAME"
-
 	echo -e "\nPull Request URL - $PR_URL"
 	echo "[i] Pull Request Number - $PR_NUMBER"
 
