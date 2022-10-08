29dfda41e9fc1101cb3846b32326d7e6fadf2ab9
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-08 14:53:07 +0200

    Try to fix the script
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_pull_close.sh
+++ .github/scripts/common_pull_close.sh
@@ -1,3 +1,5 @@
+PULL_REQUEST_OPENED=0
+
 if [[ "$PR_URL" == "" ]] && [[ "$PR_NUMBER" == "" ]]
 then
 	echo "[i] No changes in the code, deployment are allowed"
@@ -6,5 +8,7 @@ else
 	echo "[i] Pull Request Number - $PR_NUMBER"
 
 	echo -e "\n[i] Stopping the workflow to let the pull request to be approved"
-	exit 1
+	PULL_REQUEST_OPENED=1
 fi
+
+echo "::set-output name=pull_request_opened::$PULL_REQUEST_OPENED"
