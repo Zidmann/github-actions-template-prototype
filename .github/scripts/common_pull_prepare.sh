0805ce6b395b1944628ae368434ec19b958f525c
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-09 12:30:51 +0200

    Minor change
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_pull_prepare.sh
+++ .github/scripts/common_pull_prepare.sh
@@ -1,4 +1,9 @@
+echo "[i] Creating temporary checking directory"
 mkdir sources-check/
+
+echo "[i] Copying changes in the temporary checking directory"
 git clone "git@github.com:$GITHUB_REPOSITORY.git" --branch "$BRANCH_NAME" sources-check/
 cp -r sources-check/* sources/
+
+echo "[i] Removing temporary checking directory"
 rm -rf sources-check/
