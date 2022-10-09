bb0f2f4c7d5dafdd8ed4a9741a1b02ae36583013
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-09 21:29:37 +0200

    New changes
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

---
+++ module/dir2/variable.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "The project ID where the bucket will be created in"
+  type        = string
+  default     = null
+}
+
+variable "project_region" {
+  description = "The region where the bucket will be created in"
+  type        = string
+  default     = null
+}
+
+variable "project_zone" {
+  description = "The zone where the bucket will be created in"
+  type        = string
+  default     = null
+}
