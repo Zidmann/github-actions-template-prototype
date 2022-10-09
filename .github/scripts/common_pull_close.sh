0f8443e60fcf5a6a17b0e9ad3ee08642c9014dc7
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-09 12:25:34 +0200

    Minor change
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_pull_close.sh
+++ .github/scripts/common_pull_close.sh
@@ -4,7 +4,7 @@ if [[ "$PR_URL" == "" ]] && [[ "$PR_NUMBER" == "" ]]
 then
 	echo "[i] No changes in the code, deployment are allowed"
 else
-	echo -e "\nPull Request URL - $PR_URL"
+	echo -e "\n[i] Pull Request URL - $PR_URL"
 	echo "[i] Pull Request Number - $PR_NUMBER"
 
 	echo -e "\n[i] Stopping the workflow to let the pull request to be approved"
