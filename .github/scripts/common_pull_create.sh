7fac2bf45c58726dbc0551533735c55ae1c1cd29
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-09 19:13:27 +0200

    Try to correct
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_pull_create.sh
+++ .github/scripts/common_pull_create.sh
@@ -1,15 +1,19 @@
 echo "[i] Changing directory"
 cd sources/
 
-echo "[i] Comparing the $BASE_BRANCH_NAME and $BRANCH_NAME branches"
+echo "[i] Pulling the $BRANCH_NAME branch"
 git remote add originssh "git@github.com:$GITHUB_REPOSITORY.git"
 git pull originssh "$BRANCH_NAME"
 git checkout "$BRANCH_NAME"
-NB_DIFF=$(git diff "$BASE_BRANCH_NAME" "$BRANCH_NAME")
+
+echo "[i] Comparing the $BASE_BRANCH_NAME and $BRANCH_NAME branches"
+NB_DIFF=$(git diff "$BASE_BRANCH_NAME" "$BRANCH_NAME" | wc -l)
 if [ "$NB_DIFF" == "0" ]
 then
 	echo "[i] No changes to merge in a pull request"
 	exit 0
+else
+	echo "[i] Changes detected to merge"
 fi
 
 echo "[i] Creating a pull request"
