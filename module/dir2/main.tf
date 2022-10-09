f68346fee211c00bb64eee4c757ee737823cc67d
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-09 21:25:08 +0200

    New tests
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- module/dir2/main.tf
+++ module/dir2/main.tf
@@ -3,3 +3,11 @@ provider "google" {
   region  = var.project_region
   zone    = var.project_zone
 }
+
+
+resource "google_storage_bucket" "bucket" {
+  name          = "zidmann-personal-test-prototype"
+  location      = "EU"
+  project       = var.project_id
+  storage_class = "MULTI_REGIONAL"
+}
