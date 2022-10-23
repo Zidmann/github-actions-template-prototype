f8d538ada0c655deb6565af29dfc9034532d070c
GitHub Robot <github-robot@github.com[noreply]>
2022-10-23 23:23:53 +0000

    Change in module/dir1 directory

--- .github/scripts/function_replace_variables.sh
+++ .github/scripts/function_replace_variables.sh
@@ -1,6 +1,6 @@
 FILE_PATH="$*"
 TMP_PATH="/tmp/function_replace_variables.tmp"
 
-envsubst < "$FILE_PATH" > "$TMP_PATH"
-cp "$TMP_PATH" "$FILE_PATH"
-rm "$TMP_PATH"
+envsubst < "" > ""
+cp "" ""
+rm ""
