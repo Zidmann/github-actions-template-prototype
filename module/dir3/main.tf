662c94169ecf14f8f3daf14cd9b9e4eb8e164a4c
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-23 16:51:16 +0200

    Try to correct
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- module/dir3/main.tf
+++ module/dir3/main.tf
@@ -5,5 +5,9 @@ provider "google" {
 }
 
 module "module" {
-  source = "$GITHUB_REPOSITORY/module/dir1"
+  source = "git@github.com:$GITHUB_REPOSITORY.git//module/dir1"
+
+  project = var.project_id
+  region  = var.project_region
+  zone    = var.project_zone
 }
