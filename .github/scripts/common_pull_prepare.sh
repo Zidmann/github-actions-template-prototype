f7551331402bef98d582b1afbea03020df5d8d62
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-24 00:31:00 +0200

    New tests
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_pull_prepare.sh
+++ .github/scripts/common_pull_prepare.sh
@@ -2,7 +2,7 @@ echo "[i] Creating temporary checking directory"
 mkdir sources-check/
 
 echo "[i] Copying changes in the temporary checking directory"
-git clone "https://$USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" --branch "$BRANCH_NAME" sources-check/
+git clone "git@github.com/$GITHUB_REPOSITORY.git" --branch "$BRANCH_NAME" sources-check/
 cp -r sources-check/* sources/
 
 echo "[i] Removing temporary checking directory"
