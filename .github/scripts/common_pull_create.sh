700cffa8ed6d2afa5de631931b726cb88fa9f989
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-24 22:02:08 +0200

    New test
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_pull_create.sh
+++ .github/scripts/common_pull_create.sh
@@ -1,3 +1,5 @@
+cat ~/.ssh/known_hosts
+
 echo "[i] Changing directory"
 cd sources/
 
@@ -39,5 +41,3 @@ echo "[i] Exporting the pull request information"
 PR_NUMBER=${PR_URL##*/}
 echo "pr_url=$PR_URL" | tee -a $GITHUB_OUTPUT
 echo "pr_number=$PR_NUMBER" | tee -a $GITHUB_OUTPUT
-
-cat ~/.ssh/known_hosts
