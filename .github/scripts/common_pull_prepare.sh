405806e8b0a6d9ce5793dd7e584f1fd9fed9ea9b
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-24 22:47:22 +0200

    Update
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/common_pull_prepare.sh
+++ .github/scripts/common_pull_prepare.sh
@@ -4,7 +4,7 @@ mkdir sources-check/
 
 echo "-------------------------"
 echo "[i] Copying changes in the temporary checking directory"
-git clone "git@github.com/$GITHUB_REPOSITORY.git" --branch "$BRANCH_NAME" sources-check/
+git clone "git@github.com:$GITHUB_REPOSITORY.git" --branch "$BRANCH_NAME" sources-check/
 cp -r sources-check/* sources/
 
 echo "-------------------------"
