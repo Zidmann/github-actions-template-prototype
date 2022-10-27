74db3d4f62cda884f5a14a69166fcb290bacfcfc
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-28 00:56:55 +0200

    Correct
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/terraform_check.sh
+++ .github/scripts/terraform_check.sh
@@ -1,3 +1,10 @@
+if [ -f "backend.tf" ]
+then
+	echo "-------------------------"
+	echo "[i] Disabling the backend"
+	mv backend.tf backend._tf
+fi
+
 echo "-------------------------"
 echo "[i] Initiating the Terraform project"
 terraform init
@@ -5,3 +12,10 @@ terraform init
 echo "-------------------------"
 echo "[i] Validate the content of the Terraform project"
 terraform validate -no-color
+
+if [ -f "backend._tf" ]
+then
+	echo "-------------------------"
+	echo "[i] Enabling the backend"
+	mv backend._tf backend.tf
+fi
