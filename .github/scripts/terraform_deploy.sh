77a4200b01f6be3ddb87a127741b2acd536028c7
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-27 23:34:50 +0200

    Correct
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/terraform_deploy.sh
+++ .github/scripts/terraform_deploy.sh
@@ -4,7 +4,7 @@ terraform init -backend-config="bucket=$STATE_GCP_BUCKET" -backend-config="prefi
 
 echo "-------------------------"
 echo "[i] Printing the Terraform plan"
-terraform plan -no-color
+terraform plan -no-color -var "project_id=$PROJECT_ID" -var "suffix_name=$SUFFIX_NAME"
 
 echo "-------------------------"
 echo "[i] Applying the Terraform changes"
