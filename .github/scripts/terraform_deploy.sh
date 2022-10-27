1672cf2bfc0ac81e6aef7d69f832a3f69e5f6441
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-28 01:25:12 +0200

    Fix
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/terraform_deploy.sh
+++ .github/scripts/terraform_deploy.sh
@@ -1,6 +1,6 @@
 echo "-------------------------"
 echo "[i] Initiating the Terraform project"
-terraform init -backend-config="bucket=$STATE_GCP_BUCKET" -backend-config="prefix=terraform/state/$GITHUB_REPOSITORY/$WORKING_DIRECTORY"
+terraform init -backend-config="bucket=$STATE_GCP_BUCKET" -backend-config="prefix=terraform/state/$GITHUB_REPOSITORY/$WORKING_DIRECTORY/$GITHUB_SHA7"
 
 echo "-------------------------"
 echo "[i] Printing the Terraform plan"
