fc2c850b0a4535f5b18cc9d27c3294760e982422
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-10-28 09:55:14 +0200

    Add deployment on production
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- .github/scripts/terraform_deploy.sh
+++ .github/scripts/terraform_deploy.sh
@@ -1,6 +1,6 @@
 echo "-------------------------"
 echo "[i] Initiating the Terraform project"
-terraform init -backend-config="bucket=$STATE_GCP_BUCKET" -backend-config="prefix=terraform/state/$GITHUB_REPOSITORY/$WORKING_DIRECTORY/$GITHUB_SHA7"
+terraform init -backend-config="bucket=$STATE_GCP_BUCKET" -backend-config="prefix=terraform/state/$GITHUB_REPOSITORY/$WORKING_DIRECTORY/$STATE_KEY"
 
 echo "-------------------------"
 echo "[i] Printing the Terraform plan"
