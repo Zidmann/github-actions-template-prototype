b9434be55cd39b099f3865a51df993dfd40d2e77
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-11-02 21:54:14 +0100

    Minor
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- ga-pr-diff/pull_close.sh
+++ ga-pr-diff/pull_close.sh
@@ -7,11 +7,13 @@ if [[ "$PR_URL" == "" ]] && [[ "$PR_NUMBER" == "" ]]
 then
 	echo "[i] No changes in the code, deployments are allowed"
 else
-	echo -e "\n[i] Pull Request URL - $PR_URL"
+	echo "[i] Pull Request URL - $PR_URL"
 	echo "[i] Pull Request Number - $PR_NUMBER"
 
-	echo -e "\n[i] Stopping the workflow to let the pull request to be approved"
+	echo "-------------------------"
+	echo "[i] Stopping the workflow to let the pull request to be approved"
 	PULL_REQUEST_OPENED=1
 fi
 
+echo "-------------------------"
 echo "pull_request_opened=$PULL_REQUEST_OPENED" | tee -a "$GITHUB_OUTPUT"
