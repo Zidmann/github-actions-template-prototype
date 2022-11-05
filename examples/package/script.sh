24864d83fc32e3c2a2a2f92af05a704ec33639bf
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-11-05 19:12:15 +0100

    Add tests for the Shell
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

---
+++ examples/package/script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##################################################################################
+## AUTHOR : Emmanuel ZIDEL-CAUFFET - Zidmann (emmanuel.zidel@gmail.com)
+##################################################################################
+## 2021/11/05 - First release of the script
+##################################################################################
+
+
+##################################################################################
+# Beginning of the script - definition of the variables
+##################################################################################
+SCRIPT_VERSION="0.0.8"
+
+# Return code
+RETURN_CODE=0
+
+NAME_PARAM="$1"
+if [ "$NAME_PARAM" == "" ]
+then
+	echo "Usage : $0 <NAME_PARAM>"
+	exit "$RETURN_CODE"
+fi
+
+echo "Hello $NAME_PARAM"
+
+exit 0
---
+++ ga-build-package/build_package.sh
@@ -0,0 +1,21 @@
+ set -e
+
+if [ "$WORKING_DIRECTORY" != "" ]
+then
+	echo "-------------------------"
+	echo "[i] Moving to the shell directory ($WORKING_DIRECTORY)"
+	cd "$WORKING_DIRECTORY"
+fi
+
+echo "-------------------------"
+if [ "$PACKAGE_TYPE" == "DEB" ]
+then
+	echo "[i] Compile the directory in a debian package"
+	dpkg-deb --build . "$PACKAGE_FILENAME"
+elif [ "$PACKAGE_TYPE" == "RPM" ]
+then
+	echo "[i] Compile the directory in a RedHat package"
+else
+	echo "[-] Unknown package type"
+	exit 1
+fi
