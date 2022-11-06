b7978661662e1cc772d6230bb24c49bbecf1b80b
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2022-11-06 15:55:00 +0100

    Complete the ga-build-package
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- ga-build-package/build_package.sh
+++ ga-build-package/build_package.sh
@@ -10,6 +10,9 @@ fi
 echo "-------------------------"
 if [ "$PACKAGE_TYPE" == "DEB" ]
 then
+	echo "[i] Replace the variables by values"
+	sed "s/\$PACKAGE_VERSION/$PACKAGE_VERSION/g" "DEBIAN/control"
+
 	echo "[i] Compile the directory in a debian package"
 	dpkg-deb --build . "$PACKAGE_FILENAME"
 elif [ "$PACKAGE_TYPE" == "RPM" ]
